a54f6d00023535f239ebae7c467b1ecc79ed074d
kaymua <AB@Ahmads-MacBook-Air.local>
2015-11-20 20:55:59 +0100

    Created the functions
    Created two functions makeCacheMatrix and cacheSolve to return the
    inverse of a matrix.

--- cachematrix.R
+++ cachematrix.R
@@ -1,13 +1,46 @@
-## Pushing to git
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(x = matrix()) { #creating funciton that takes a matrix as a paramter and returns a list
+
+        m <- NULL #create an empty variable called m
+        
+        set <- function(y) { #set is a nested function which takes one parameter
+                x <<- y #here we do the cache, so that y == matrix
+                m <<- NULL #inverse is still null
+        }
+        
+        get <- function() {
+                x #return x
+        }
+        
+        setinverse <- function(inverse) {
+                m <<- inverse #set m to be the inverse
+        }
+        
+        getinverse <- function() {
+                m #return m
+        }
+        
+        #creating a list with 4 functions: set, get, setinverse and getinverse
+        list(set = set, 
+             get = get,
+             setinverse = setinverse,
+             getinverse = getinverse
+             )
 
 }
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x) { #create a function to return a matrix that is the inverse of 'x'
+        
+                m <- x$getinverse() #calls our function getinverse and assigns the value to m
+                if(!is.null(m)) { #if m is not null, we return the value of m
+                        message("pulling cached matrix") #friendly message to user
+                        return(m) #return value of m
+                }
+                
+                data <- x$get()
+                m <- solve(data)
+                x$setinverse(m) #get the inverse of m
+                m #return m
+        
 }
